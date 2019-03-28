7013607cc7d270545a743d0778a4761acc95e795
Cathy <cathywu1983@gmail.com>
2019-03-29 01:34:44 +0800

    add UI

---
+++ src/main/java/tech/cathywu/excelreader/ui/MainFrame.java
@@ -0,0 +1,65 @@
+package tech.cathywu.excelreader.ui;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainFrame extends JFrame implements ActionListener {
+
+    private final JLabel filePathLabel;
+
+    private List<String> fileList = new ArrayList<>();
+
+    public MainFrame() {
+        super("Upload Excel");
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        panel.add(new UploadButton(this), BorderLayout.NORTH);
+        filePathLabel = new JLabel("");
+        panel.add(filePathLabel, BorderLayout.CENTER);
+
+        this.add(panel);
+    }
+
+    public static void main(String[] args) {
+        // Create and set up a frame window
+        MainFrame cl = new MainFrame();
+        cl.setSize(400, 200);
+        cl.setVisible(true);
+        cl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        int returnValue = jfc.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = jfc.getSelectedFile();
+            String absolutePath = selectedFile.getAbsolutePath();
+            fileList.add(absolutePath);
+            System.out.println("file selected: " + absolutePath);
+            drawFileListLabel();
+        }
+    }
+
+    private void drawFileListLabel() {
+
+        StringBuffer content = new StringBuffer();
+        content.append("<html>");
+
+        fileList.forEach(file -> content.append("<pre>").append(file).append("</pre><br/>"));
+
+        content.append("</html>");
+
+        filePathLabel.setText(content.toString());
+
+        filePathLabel.repaint();
+    }
+}
---
+++ src/main/java/tech/cathywu/excelreader/ui/UploadButton.java
@@ -0,0 +1,14 @@
+package tech.cathywu.excelreader.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class UploadButton extends JButton {
+
+    public UploadButton(ActionListener actionListener) {
+        super("Choose File");
+        this.setBackground(Color.BLUE);
+        this.addActionListener(actionListener);
+    }
+}
